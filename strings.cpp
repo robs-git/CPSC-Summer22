0370f5c39a75032540079193eb137757bb10dcac
robs.git <robert2189@gmail.com>
2022-06-07 14:12:06 -0600

    save

---
+++ strings.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main () {
+
+    string a;
+
+
+    a = "hello this is a string";
+
+    cout << a << endl;
+
+
+    cout << "length of string: " << a.length() << endl;
+
+    cout << "find hello in string: " << a.find("hello")<< endl;
+
+    cout << "find string in string: " << a.find("string")<< endl;
+
+    cout << "string at character 0: " << a[0]<< endl;
+
+}
