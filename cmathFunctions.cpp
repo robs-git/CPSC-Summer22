72eb932bba84ba400c6aff988b49edb0ec262174
robs.git <robert2189@gmail.com>
2022-06-07 13:20:53 -0600

    update

---
+++ cmathFunctions.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    int x, y = 1, result;
+
+
+    //5y^2 - 2y
+
+
+    // 5y
+    5*y;
+
+
+    // 5y^2
+    cout << "5y^2: " << pow((5*y),2) << endl;
+
+    // 2y
+    2*y;
+
+    cout << "5y^2 - 2y: " << pow((5*y),2) - 2*y << endl;
+
+    return 0; 
+
+}
