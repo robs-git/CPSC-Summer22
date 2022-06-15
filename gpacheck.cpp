2b1a95b960e2363d556dab89b3e0ccabfe9f1b1e
robs.git <robert2189@gmail.com>
2022-06-14 21:07:47 -0600

    commit

--- gpacheck.cpp
+++ gpacheck.cpp
@@ -5,35 +5,60 @@
 
 
 using namespace std;
-void obtainNamesAndGPAs(ifstream& fin, string Name[], double GPA[]);
+void obtainNamesAndGPAs(ifstream& fin, string Name[], double GPA[], int& count); // added count to the end
+
+void printNamesGPAs(string Name[], double GPA[], int count);
 const int SIZE = 200;
 
 
-int main(){
+int main() {
+
+
 ifstream fin;
+string Name[SIZE];
+double GPA[SIZE];
+int count; // added count to keep track of number of students in GPA.TXT
+
 fin.open("GPA.txt");
-string Name [SIZE];
-double GPA [SIZE];
-obtainNamesAndGPAs(fin, Name, GPA);
+
+obtainNamesAndGPAs(fin, Name, GPA, count);
+
+printNamesGPAs(Name,GPA,count);
 
 
+    fin.close();
+ 
 
-    return 0;
+     return 0;
 }
 
 
 
-void obtainNamesAndGPAs(ifstream& fin, string Name[], double GPA[]) {
-int i = 0;
+void obtainNamesAndGPAs(ifstream& fin, string Name[], double GPA[], int& count) {
 
-getline(fin, Name[i]);
-fin >> GPA[i];
-while (!fin.eof()){
-    i++;
-getline(fin, Name[i]);
-fin >> GPA[i];
+int i = 0; //to iterate through do loop
+
+do {
+    getline(fin,Name[i]); //get entire line for string name
+    fin >> GPA[i]; // get double amount
+     i++; //iterate i;
+
+
+}while(fin.get() != EOF);
+
+count = i - 1; //reduce by 1 as array does 1 loop after last line of file
 
 }
 
-cout << i;
+
+
+void printNamesGPAs(string Name[], double GPA[], int count) {
+
+
+    for (int i = 0; i < count; i++) {
+
+        cout << Name[i] << endl;
+        cout << GPA[i] << endl;
+    }
 }
+
